6d79c59eaa74c79a05c364e9ae0e45fba3f36e58
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 09:51:51 +0200

    ready to handle event

--- cobalt.actionpicker.js
+++ cobalt.actionpicker.js
@@ -1,16 +1,22 @@
-(function(cobalt){
-    var plugin={
-        classes:{
-			ios: "ActionPickerPlugin",
-            android: "io.kristal.actionpicker.ActionPicker"
-        },
-        init:function(){
-            cobalt.actionPicker=this.getAction.bind(this);
-        },
-        getAction:function(option,callback){
-            cobalt.plugins.send(this, "getAction", option, callback);
-        }
-	};
-    cobalt.plugins.register(plugin);
+(function(cobalt) {
+  var plugin = {
+    classes: {
+      ios: "ActionPickerPlugin",
+      android: "io.kristal.actionpicker.ActionPicker"
+    },
+    init: function() {
+      cobalt.actionPicker = this.getAction.bind(this);
+    },
+    getAction: function(option, callback) {
+      cobalt.actionPickerCallback = callback;
+      cobalt.plugins.send(this, "getAction", option);
+    },
+    handleEvent: function(json) {
+      if (typeof cobalt.actionPickerCallback === 'function') {
+        cobalt.actionPickerCallback(json.index)
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 
 })(cobalt || {});
