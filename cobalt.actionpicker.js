e5e48db42c3bd2fd21b54825680131ec862b9d37
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-18 15:27:28 +0200

    using cobalt.plugins.send with callback

--- cobalt.actionpicker.js
+++ cobalt.actionpicker.js
@@ -8,19 +8,8 @@
     init: function() {
       cobalt.actionPicker = this.getAction.bind(this);
     },
-    callbacks: {},
-    randomId : function(){
-      return Math.random().toString(36).substr(2, 10);
-    },
     getAction: function(option, callback) {
-      option.callback = this.randomId();
-      this.callbacks[option.callback] = callback;
-      cobalt.plugins.send(this, "getAction", option);
-    },
-    handleEvent: function(json) {
-      if (json.data && json.data.callback && typeof this.callbacks[json.data.callback] === 'function') {
-        this.callbacks[json.data.callback](json.data.index);
-      }
+      cobalt.plugins.send(this, "getAction", option, callback);
     }
   };
   cobalt.plugins.register(plugin);
